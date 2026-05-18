d13c17b6e4a2772c8692c0014aec9252db91e39b
Ivan Kinder <kinder.wit@gmail.com>
2026-05-18 11:30:14 +0300

    chore(dashboard): migrate ESLint config to flat config

---
+++ module/iron_dashboard/eslint.config.mjs
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  },
+  eslintConfigPrettier,
+  {
+    rules: {},
+  },
+]
